5fbb21699ec61dc260ff139e65ad77f13a0e5e05
Francis Vitt <fvitt@ucar.edu>
2024-10-04 10:37:54 -0600

    some clean up
    	modified:   src/ionosphere/waccmx/edyn3D_heelis.F90

--- src/ionosphere/waccmx/edyn3D_heelis.F90
+++ src/ionosphere/waccmx/edyn3D_heelis.F90
@@ -1,3 +1,6 @@
+!-----------------------------------------------------------------------
+!  Heelis high-latitude empirical electric potential for 3D dynamo
+!-----------------------------------------------------------------------
 module edyn3D_heelis
 
   use shr_kind_mod, only: r8 => shr_kind_r8
@@ -10,13 +13,14 @@ module edyn3D_heelis
 
 contains
 
+  ! set poten_hl for edyn solver input
   subroutine edyn3D_heelis_set_hlat_pot(sunlon)
 
     use edyn3D_fieldline, only: poten_hl ! (0:nmlon+1,nmlat_T1)
     use edyn3D_params, only: nmlon, nmlat_h, nmlat_T1, ylatm, ylonm
     use heelis_mod,only: heelis_update, heelis_flwv32
 
-    real(r8),intent(in) :: sunlon
+    real(r8),intent(in) :: sunlon ! mag longitude sun location
 
     real(r8) :: xlat(nmlon)
     real(r8) :: xlon(nmlon)
@@ -27,27 +31,27 @@ contains
     call heelis_update()
 
     ratio(:) = 1._r8
+    poten_hl(:,:) = 0._r8
 
     do isn = 1,2
        do j = 1,nmlat_h
-          if (isn==1) then
-             jj = j
-          else
-             jj = nmlat_T1 - j + 1
-          end if
-
-          xlat(:) = ylatm(j,isn)
-          xlon(:) = ylonm(1:nmlon)-sunlon
-
           if (abs(ylatm(j,isn)) > pi/6._r8) then
-             call heelis_flwv32(xlat,xlon,ratio,pi,iflag,nmlon,poten_hl(1:nmlon+1,jj))
-          else
-             poten_hl(1:nmlon,jj) = 0._r8
-          end if
+             if (isn==1) then
+                jj = j
+             else
+                jj = nmlat_T1 - j + 1
+             end if
 
+             xlat(:) = ylatm(j,isn)
+             xlon(:) = ylonm(1:nmlon)-sunlon
+             iflag(:) = 1 ! must be updated at each j
+
+             call heelis_flwv32(xlat,xlon,ratio,pi,iflag,nmlon,poten_hl(1:nmlon+1,jj))
+          end if
        end do
     end do
 
+    ! wrap around longitude point
     poten_hl(0,:) = poten_hl(nmlon,:)
 
   end subroutine edyn3D_heelis_set_hlat_pot
