28e0530fd9bbc462445ae24b766293b5f4f89a3b
tommelt <thomas.meltzer1@gmail.com>
2025-08-20 10:20:37 -0600

    wip: try to fix corner issue in regrid

--- src/utils/esmf_phys2lonlat_mod.F90
+++ src/utils/esmf_phys2lonlat_mod.F90
@@ -13,7 +13,7 @@ module esmf_phys2lonlat_mod
   use ESMF, only: ESMF_FieldCreate, ESMF_FieldRegridStore
   use ESMF, only: ESMF_FieldGet, ESMF_FieldRegrid
   use ESMF, only: ESMF_KIND_I4, ESMF_KIND_R8, ESMF_TYPEKIND_R8
-  use ESMF, only: ESMF_REGRIDMETHOD_BILINEAR, ESMF_POLEMETHOD_ALLAVG, ESMF_EXTRAPMETHOD_NEAREST_IDAVG
+  use ESMF, only: ESMF_REGRIDMETHOD_BILINEAR, ESMF_POLEMETHOD_NONE, ESMF_EXTRAPMETHOD_NEAREST_IDAVG
   use ESMF, only: ESMF_TERMORDER_SRCSEQ, ESMF_MESHLOC_ELEMENT, ESMF_STAGGERLOC_CENTER
   use ESMF, only: ESMF_FieldDestroy, ESMF_RouteHandleDestroy
   use esmf_check_error_mod, only: check_esmf_error
@@ -33,9 +33,11 @@ module esmf_phys2lonlat_mod
 
   type(ESMF_Field) :: physfld_3d
   type(ESMF_Field) :: lonlatfld_3d
+  type(ESMF_Field) :: lonlatfld_3d_copy
 
   type(ESMF_Field) :: physfld_2d
   type(ESMF_Field) :: lonlatfld_2d
+  type(ESMF_Field) :: lonlatfld_2d_copy
 
   interface esmf_phys2lonlat_regrid
      module procedure esmf_phys2lonlat_regrid_2d
@@ -84,6 +86,7 @@ contains
     lonlatfld_3d = ESMF_FieldCreate( lonlat_grid, arrayspec, staggerloc=ESMF_STAGGERLOC_CENTER, &
                                      ungriddedLBound=(/1,1/), ungriddedUBound=(/pver,nflds/), rc=rc)
     call check_esmf_error(rc, subname//'ESMF_FieldCreate 3D lonlat fld ERROR')
+    lonlatfld_3d_copy = lonlatfld_3d
 
     ! 2D phys fld
     call ESMF_ArraySpecSet(arrayspec, 1, ESMF_TYPEKIND_R8, rc=rc)
@@ -99,19 +102,20 @@ contains
 
     lonlatfld_2d = ESMF_FieldCreate( lonlat_grid, arrayspec, staggerloc=ESMF_STAGGERLOC_CENTER, rc=rc)
     call check_esmf_error(rc, subname//'ESMF_FieldCreate 2D lonlat fld ERROR')
+    lonlatfld_2d_copy = lonlatfld_2d
 
-    call ESMF_FieldRegridStore(srcField=physfld_3d, dstField=lonlatfld_3d, &
+    call ESMF_FieldRegridStore(srcField=physfld_3d, dstField=lonlatfld_3d_copy, &
          regridMethod=ESMF_REGRIDMETHOD_BILINEAR,                          &
-         polemethod=ESMF_POLEMETHOD_ALLAVG,                                &
+         polemethod=ESMF_POLEMETHOD_NONE,                                &
          extrapMethod=ESMF_EXTRAPMETHOD_NEAREST_IDAVG,                     &
          routeHandle=rh_phys2lonlat_3d, factorIndexList=factorIndexList,   &
          factorList=factorList, srcTermProcessing=smm_srctermproc,         &
          pipelineDepth=smm_pipelinedep, rc=rc)
     call check_esmf_error(rc, subname//'ESMF_FieldRegridStore 3D routehandle ERROR')
 
-    call ESMF_FieldRegridStore(srcField=physfld_2d, dstField=lonlatfld_2d, &
+    call ESMF_FieldRegridStore(srcField=physfld_2d, dstField=lonlatfld_2d_copy, &
          regridMethod=ESMF_REGRIDMETHOD_BILINEAR,                          &
-         polemethod=ESMF_POLEMETHOD_ALLAVG,                                &
+         polemethod=ESMF_POLEMETHOD_NONE,                                &
          extrapMethod=ESMF_EXTRAPMETHOD_NEAREST_IDAVG,                     &
          routeHandle=rh_phys2lonlat_2d, factorIndexList=factorIndexList,   &
          factorList=factorList, srcTermProcessing=smm_srctermproc,         &
