986fcf8f19e6084db49bd988311d77c22bf85072
Francis Vitt <fvitt@ucar.edu>
2025-10-04 09:08:51 -0600

    interp time-ave cospectra to replace zeros
    	modified:   src/utils/cospext_mod.F90

--- src/utils/cospext_mod.F90
+++ src/utils/cospext_mod.F90
@@ -1,4 +1,5 @@
-!  !!Psudo code that calls cospext. This can be after each cospectra is calculated in zonal_fft_mod
+!===============================================================================
+!  !! This can be after each cospectra is calculated in zonal_fft_mod
 !  !! After u_fft*wstar
 !  call cospext(cspr,wvlong,wvlxbeg,wvlxend,ntime,mflxxup,mflxxun)
 !  !! After v_fft*wstar
@@ -21,7 +22,8 @@
 !  - write / read acculated coespectras to / from IC and restart files
 !  - compute forcings as described above on "zonal" grid
 !  - scatter forcings to physics grid
-!  ...
+!
+!===============================================================================
 
 module cospext_mod
   use shr_kind_mod, only: r8 => SHR_KIND_R8
@@ -34,12 +36,13 @@ module cospext_mod
 
 contains
 
+  !=============================================================================
   !! This subroutine should be called once per day. The cospectra should be collected either every hour (if the calculation is
   !! is expensive), or every time step (if the calculation is not so expensive), or somewhere in between.
   !! Will need to save th daily forcing on IC (.i.) file and restart file (.r*), so GW forcing is available for startup and
   !! restart runs, including short (less one day) runs. The daily forcing should also be made optional for the IC file.
   !! The run can still start without the forcing.
-
+  !=============================================================================
   subroutine cospext(nftnum,lat_beg,lat_end, pver,ntime, latrad, cspr, wvlxbeg,wvlxend, mflxup,mflxun)
 
     integer, intent(in) :: nftnum,lat_beg,lat_end,pver,ntime
@@ -67,6 +70,13 @@ contains
     integer :: kxl(lat_beg:lat_end), kxm(lat_beg:lat_end), kxr(lat_beg:lat_end), kxbeg(lat_beg:lat_end), kxend(lat_beg:lat_end)
     integer :: j
     real(r8) :: csprp(nftnum, lat_beg:lat_end, pver),csprn(nftnum, lat_beg:lat_end, pver)
+    real(r8) :: csprpi(nftnum, lat_beg:lat_end, pver),csprni(nftnum, lat_beg:lat_end, pver)
+
+    real(r8) :: r2d = 180._r8/pi
+    integer :: lat0, lat1
+
+    mflxup = 0._r8
+    mflxun = 0._r8
 
     do j = lat_beg,lat_end
        circlat(j) = 2._r8 * pi * rearth * cos(latrad(j))     !! rearth is Earth radius in m
@@ -75,161 +85,251 @@ contains
     kxbeg(:)  = nint(circlat(:)/wvlxbeg)
     kxend(:)  = nint(circlat(:)/wvlxend)
 
-!! These following 3 lines calculate the scale invariance range according to the short wavelength of the resolved range
+    !! These following 3 lines calculate the scale invariance range according to the short wavelength of the resolved range
     kxr(:) = kxbeg(:)
     kxm(:) = kxr(:)/2
     kxl(:) = kxm(:)/2
 
-    !! The following calculation should be done equatorward of 85 deg latitude. A conditional should be set in the subroutine
+    lat0 = 0
+    lat1 = -1
 
-    call spectral_separate(nftnum,lat_beg,lat_end,pver,ntime, cspr,csprp,csprn)  !! separate the cospectra into positive and negative branches from the accumulated spectra
-    call spectral_slope(nftnum,lat_beg,lat_end,pver,csprp,kxl,slpp)
-    call spectral_slope(nftnum,lat_beg,lat_end,pver,-csprn,kxl,slpn)
-    call scale_unres(nftnum,lat_beg,lat_end,pver,kxl,kxbeg,kxend,csprp,csprn,slpp,slpn,mflxup,mflxun)
-
-    return
-  end subroutine cospext
-
-
-  subroutine spectral_separate(nftnum,lat_beg,lat_end,pver,ntime,cspr,csprp,csprn)
-
-    integer, intent(in) :: nftnum,lat_beg,lat_end,pver,ntime
-    real(r8), intent(in) :: cspr(nftnum, lat_beg:lat_end, pver, ntime)  !! co-spectra resolved by the model
-
-    !! csprp: cospectra that is positive and averaged over the accumulated time period
-    !! csprn: cospectra that is negative and averaged over the accumulated time period
-    real(r8), intent(out) :: csprp(nftnum, lat_beg:lat_end, pver),csprn(nftnum, lat_beg:lat_end, pver)
-
-    !! csprps: cospectra that is positive at each time slice
-    !! csprns: cospectra that is negative at each time slice
-    real(r8) :: csprps(nftnum,lat_beg:lat_end,pver,ntime),csprns(nftnum,lat_beg:lat_end,pver,ntime)
-    real(r8) :: wrk1(ntime)
-    integer :: i,j,k
-    integer :: npos, nneg
-
-    csprps = 0._r8
-    csprns = 0._r8
-    csprp = 0._r8
-    csprn = 0._r8
-
-    !! separate out the positve and negative spectral component at each time step
-
-    where(cspr>0._r8)
-       csprps = cspr
-    end where
-    where(cspr<0._r8)
-       csprns = cspr
-    end where
-
-    !! Find the average positive and negative cospectral components
-
-    do k = 1, pver
-       do j = lat_beg, lat_end
-          do i = 1, nftnum
-
-             wrk1 = csprps(i, j, k, :)
-             npos = count(wrk1/=0._r8)
-             if (npos /= 0) then
-                csprp(i, j, k) = sum(wrk1) / real(npos,kind=r8)
-             end if
-
-             wrk1 = csprns(i, j, k, :)
-             nneg = count(wrk1/=0._r8)
-             if (nneg /= 0) then
-                csprn(i, j, k) = sum(wrk1) / real(nneg,kind=r8)
-             end if
-
-          end do
-       end do
+    !! calculations are done equatorward of 85 deg latitude
+    do j = lat_beg,lat_end
+       if ( abs(latrad(j)*r2d) < 85._r8 ) then
+          if (lat0<1) lat0 = j
+          lat1 = j
+       end if
     end do
-    return
-  end subroutine spectral_separate
 
-  subroutine spectral_slope(nftnum,lat_beg,lat_end,pver,spct,kxl,slp)
-    integer, intent(in) :: nftnum,lat_beg,lat_end,pver
-    real(r8), intent(in) :: spct(nftnum,lat_beg:lat_end,pver)
-    integer, intent(in) :: kxl(lat_beg:lat_end)
-    real(r8), intent(out) :: slp(lat_beg:lat_end,pver)  ! spectral slope of power law over kxl to 4*kxl
+    !! separate the cospectra into positive and negative branches from the accumulated spectra
+    call spectral_separate(cspr,csprp,csprn,csprpi,csprni)
+    call spectral_slope( csprpi,kxl,slpp)
+    call spectral_slope(-csprni,kxl,slpn)
+    call scale_unres(kxl,kxbeg,kxend,csprp,csprn,slpp,slpn,mflxup,mflxun)
 
-    real(r8) :: silm, simr  ! integration of spct over kxl to 2*kxl, and over 2*kxl to 4*kxl
-    integer :: j,k
+  contains
 
-    do k = 1,pver
-       do j=lat_beg,lat_end
-          if (kxl(j)>0) then
-             silm = sum(spct(kxl(j):2*kxl(j),j,k))
-             simr = sum(spct(2*kxl(j):4*kxl(j),j,k))
-             if (silm>0.0_r8 .and. simr>0.0_r8) then
-                slp(j,k) = MAX(1._r8-log(simr/silm)/log(2._r8),-1._r8)
-             else
-                slp(j,k) = NOTSET
-             end if
-          end if
-       enddo
-    enddo
+    !===========================================================================
+    !===========================================================================
+    subroutine spectral_separate(cspr,csprp,csprn,csprpi,csprni)
 
-    return
-  end subroutine spectral_slope
+      !! co-spectra resolved by the model
+      real(r8), intent(in) :: cspr(nftnum, lat_beg:lat_end, pver, ntime)
 
-  subroutine scale_unres(nftnum,lat_beg,lat_end,pver,kxl,kxbeg,kxend,csprp,csprn,slpp,slpn,mflxup,mflxun)
+      !! csprp: cospectra that is positive and averaged over the accumulated time period
+      !! csprn: cospectra that is negative and averaged over the accumulated time period
+      real(r8), intent(out) :: csprp(nftnum, lat_beg:lat_end, pver)
+      real(r8), intent(out) :: csprn(nftnum, lat_beg:lat_end, pver)
 
-    integer,  intent(in)  :: nftnum,lat_beg,lat_end,pver
-    real(r8), intent(in)  :: csprp(nftnum,lat_beg:lat_end,pver)
-    real(r8), intent(in)  :: csprn(nftnum,lat_beg:lat_end,pver)
-    integer,  intent(in)  :: kxl(lat_beg:lat_end)
-    integer,  intent(in)  :: kxbeg(lat_beg:lat_end)
-    integer,  intent(in)  :: kxend(lat_beg:lat_end)
-    real(r8), intent(in)  :: slpp(lat_beg:lat_end,pver)
-    real(r8), intent(in)  :: slpn(lat_beg:lat_end,pver)
+      ! interpolated over the zeros
+      real(r8), intent(out) :: csprpi(nftnum, lat_beg:lat_end, pver)
+      real(r8), intent(out) :: csprni(nftnum, lat_beg:lat_end, pver)
 
-    real(r8), intent(out) :: mflxup(lat_beg:lat_end,pver)
-    real(r8), intent(out) :: mflxun(lat_beg:lat_end,pver)
+      !! csprps: cospectra that is positive at each time slice
+      !! csprns: cospectra that is negative at each time slice
+      real(r8) :: csprps(nftnum,lat_beg:lat_end,pver,ntime)
+      real(r8) :: csprns(nftnum,lat_beg:lat_end,pver,ntime)
+      real(r8) :: wrk1(ntime)
+      integer :: i,j,k
+      integer :: npos, nneg
 
-    real(r8) :: siresp, siresn, bp, bn, fp, fn
-    integer :: j,k
+      csprps = 0._r8
+      csprns = 0._r8
+      csprp = 0._r8
+      csprn = 0._r8
+      csprpi = 0._r8
+      csprni = 0._r8
 
-    mflxup = 0._r8
-    mflxun = 0._r8
+      !! separate out the positve and negative spectral component at each time step
 
-    do k=1,pver
-       do j=lat_beg,lat_end
-          if (kxl(j) > 2) then
-             if (slpp(j,k)/=NOTSET.and.slpp(j,k)/=1._r8) then
-                siresp = sum(csprp(kxl(j):kxbeg(j),j,k),1)
-                bp = 1._r8-slpp(j,k)
-                fp = (real(kxend(j),r8)**bp-real(kxbeg(j),r8)**bp)/(real(kxbeg(j),r8)**bp-real(kxl(j),r8)**bp)
-                mflxup(j,k) = siresp*fp
-             end if
-             if (slpp(j,k)/=NOTSET.and.slpp(j,k)==1._r8) then
-                siresp = sum(csprp(kxl(j):kxbeg(j),j,k),1)
-                fp = log(real(kxend(j),r8)/real(kxbeg(j),r8))/log(real(kxbeg(j),r8)/real(kxl(j),r8))
-                mflxup(j,k) = siresp*fp
-             end if
-             if (slpp(j,k)==NOTSET) then
-                mflxup(j,k) = 0._r8
-             end if
-                
-             if (slpn(j,k)/=NOTSET.and.slpn(j,k)/=1._r8) then
-                siresn = sum(csprn(kxl(j):kxbeg(j),j,k),1)
-                bn = 1._r8-slpn(j,k)
-                fn = (real(kxend(j),r8)**bn-real(kxbeg(j),r8)**bn)/(real(kxbeg(j),r8)**bn-real(kxl(j),r8)**bn)
-                mflxun(j,k) = siresn*fn
-             end if
-             if (slpn(j,k)/=NOTSET.and.slpn(j,k)==1._r8) then
-                siresp = sum(csprn(kxl(j):kxbeg(j),j,k),1)
-                fn = log(real(kxend(j),r8)/real(kxbeg(j),r8))/log(real(kxbeg(j),r8)/real(kxl(j),r8))
-                mflxun(j,k) = siresn*fn
-             end if
-             if (slpn(j,k)==NOTSET) then
-                mflxun(j,k) = 0._r8
-             end if
-             
-          endif
-       enddo
-    enddo
+      where(cspr>0._r8)
+         csprps = cspr
+      end where
+      where(cspr<0._r8)
+         csprns = cspr
+      end where
 
-    return
-  end subroutine scale_unres
+      !! Find the average positive and negative cospectral components
 
+      do k = 1, pver
+         do j = lat0,lat1
+            do i = 1, nftnum
+
+               wrk1 = csprps(i, j, k, :)
+               npos = count(wrk1/=0._r8)
+               if (npos /= 0) then
+                  csprp(i, j, k) = sum(wrk1) / real(npos,kind=r8)
+               end if
+
+               wrk1 = csprns(i, j, k, :)
+               nneg = count(wrk1/=0._r8)
+               if (nneg /= 0) then
+                  csprn(i, j, k) = sum(wrk1) / real(nneg,kind=r8)
+               end if
+
+            end do
+
+            ! interpolate to replace the zeros
+            csprpi(:,j,k) = interp_cospec(csprp(:,j,k))
+            csprni(:,j,k) = interp_cospec(csprn(:,j,k))
+
+         end do
+      end do
+
+    end subroutine spectral_separate
+
+    !===========================================================================
+    subroutine spectral_slope(spct,kxl,slp)
+
+      real(r8), intent(in) :: spct(nftnum,lat_beg:lat_end,pver)
+      integer, intent(in) :: kxl(lat_beg:lat_end)
+      real(r8), intent(out) :: slp(lat_beg:lat_end,pver)  ! spectral slope of power law over kxl to 4*kxl
+
+      real(r8) :: silm, simr  ! integration of spct over kxl to 2*kxl, and over 2*kxl to 4*kxl
+      integer :: j,k
+
+      do k = 1,pver
+         do j=lat0,lat1
+            if (kxl(j)>0) then
+               silm = sum(spct(kxl(j):2*kxl(j),j,k))
+               simr = sum(spct(2*kxl(j):4*kxl(j),j,k))
+               if (silm>0.0_r8 .and. simr>0.0_r8) then
+                  slp(j,k) = MAX(1._r8-log(simr/silm)/log(2._r8),-1._r8)
+               else
+                  slp(j,k) = NOTSET
+               end if
+            end if
+         enddo
+      enddo
+
+    end subroutine spectral_slope
+
+    !===========================================================================
+    subroutine scale_unres(kxl,kxbeg,kxend, csprp,csprn,slpp,slpn,mflxup,mflxun)
+
+      real(r8), intent(in)  :: csprp(nftnum,lat_beg:lat_end,pver)
+      real(r8), intent(in)  :: csprn(nftnum,lat_beg:lat_end,pver)
+      integer,  intent(in)  :: kxl(lat_beg:lat_end)
+      integer,  intent(in)  :: kxbeg(lat_beg:lat_end)
+      integer,  intent(in)  :: kxend(lat_beg:lat_end)
+      real(r8), intent(in)  :: slpp(lat_beg:lat_end,pver)
+      real(r8), intent(in)  :: slpn(lat_beg:lat_end,pver)
+
+      real(r8), intent(out) :: mflxup(lat_beg:lat_end,pver)
+      real(r8), intent(out) :: mflxun(lat_beg:lat_end,pver)
+
+      real(r8) :: siresp, siresn, bp, bn, fp, fn
+      integer :: j,k
+
+      mflxup = 0._r8
+      mflxun = 0._r8
+
+      do k=1,pver
+         do j=lat0,lat1
+            if (kxl(j) > 2) then
+               if (slpp(j,k)/=NOTSET.and.slpp(j,k)/=1._r8) then
+                  siresp = sum(csprp(kxl(j):kxbeg(j),j,k),1)
+                  bp = 1._r8-slpp(j,k)
+                  fp = (real(kxend(j),r8)**bp-real(kxbeg(j),r8)**bp)/(real(kxbeg(j),r8)**bp-real(kxl(j),r8)**bp)
+                  mflxup(j,k) = siresp*fp
+               end if
+               if (slpp(j,k)/=NOTSET.and.slpp(j,k)==1._r8) then
+                  siresp = sum(csprp(kxl(j):kxbeg(j),j,k),1)
+                  fp = log(real(kxend(j),r8)/real(kxbeg(j),r8))/log(real(kxbeg(j),r8)/real(kxl(j),r8))
+                  mflxup(j,k) = siresp*fp
+               end if
+               if (slpp(j,k)==NOTSET) then
+                  mflxup(j,k) = 0._r8
+               end if
+
+               if (slpn(j,k)/=NOTSET.and.slpn(j,k)/=1._r8) then
+                  siresn = sum(csprn(kxl(j):kxbeg(j),j,k),1)
+                  bn = 1._r8-slpn(j,k)
+                  fn = (real(kxend(j),r8)**bn-real(kxbeg(j),r8)**bn)/(real(kxbeg(j),r8)**bn-real(kxl(j),r8)**bn)
+                  mflxun(j,k) = siresn*fn
+               end if
+               if (slpn(j,k)/=NOTSET.and.slpn(j,k)==1._r8) then
+                  siresp = sum(csprn(kxl(j):kxbeg(j),j,k),1)
+                  fn = log(real(kxend(j),r8)/real(kxbeg(j),r8))/log(real(kxbeg(j),r8)/real(kxl(j),r8))
+                  mflxun(j,k) = siresn*fn
+               end if
+               if (slpn(j,k)==NOTSET) then
+                  mflxun(j,k) = 0._r8
+               end if
+
+            endif
+         enddo
+      enddo
+
+    end subroutine scale_unres
+
+    !=============================================================================
+    ! interpolate to replace zeros in the cospectra
+    function interp_cospec(cosp) result(cospi)
+      use interpolate_data, only: lininterp
+
+      real(r8), intent(in) :: cosp(nftnum)
+      real(r8) :: cospi(nftnum)
+
+      integer :: i, nzeros, n_nonzero, zcnt, ncnt
+
+      real(r8), allocatable :: zero_wave_numbers(:)
+      real(r8), allocatable :: interp_values(:)
+      real(r8), allocatable :: nonzero_wave_num(:)
+      real(r8), allocatable :: nonzero_values(:)
+      real(r8) :: sign
+
+      cospi = cosp
+      nzeros = count(cosp==0._r8)
+
+      if (any(cosp<0._r8)) then
+         sign = -1._r8
+      else
+         sign = 1._r8
+      end if
+
+      if (nzeros>0) then
+
+         n_nonzero = nftnum-nzeros
+
+         ! replace the zeros by interpolating neighbors
+         allocate(zero_wave_numbers(nzeros))
+         allocate(interp_values(nzeros))
+         allocate(nonzero_wave_num(n_nonzero))
+         allocate(nonzero_values(n_nonzero))
+         zcnt = 0
+         ncnt = 0
+         do i = 1,nftnum
+            if (cosp(i)==0._r8) then
+               zcnt = zcnt+1
+               zero_wave_numbers(zcnt) = real(i,kind=r8)
+            else
+               ncnt = ncnt+1
+               nonzero_wave_num(ncnt) = real(i,kind=r8)
+               nonzero_values(ncnt) = log(sign*cosp(i))
+            endif
+         end do
+
+         call lininterp(nonzero_values,nonzero_wave_num,n_nonzero, &
+              interp_values, zero_wave_numbers, nzeros)
+
+         zcnt = 0
+         do i = 1,nftnum
+            if (cosp(i)==0._r8) then
+               zcnt = zcnt+1
+               cospi(i) = sign*exp(interp_values(zcnt))
+            end if
+         end do
+
+         deallocate(zero_wave_numbers)
+         deallocate(interp_values)
+         deallocate(nonzero_wave_num)
+         deallocate(nonzero_values)
+
+      end if
+
+    end function interp_cospec
+
+  end subroutine cospext
 
 end module cospext_mod
